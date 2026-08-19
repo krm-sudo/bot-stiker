7c4fd2f943a78917e798adbfb2b691691f0d196c
krm-sudo <asranisatar45@gmail.com>
2026-08-19 22:36:19 +0700

    webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,28 +1,33 @@
 const axios = require('axios');
 
 module.exports = async (req, res) => {
+    // Tanggapi langsung agar tidak timeout
     if (req.method !== 'POST') {
         return res.status(405).send('Method Not Allowed');
     }
 
-    const { sender, url } = req.body;
-    
-    // Token Fonnte kamu sudah terpasang di sini
-    const FONNTE_TOKEN = 'H9RkfaEm7eLXPXB7r7fk'; 
+    const { sender, url, file } = req.body;
+    const mediaUrl = url || file;
+    const FONNTE_TOKEN = 'H9RkfaEm7eLXPXB7r7fk';
 
-    if (url) {
+    // Kirim respons 200 OK ke Fonnte
+    res.status(200).send('OK');
+
+    // Jika pesan berisi media/gambar, kirim sebagai stiker
+    if (mediaUrl) {
         try {
             await axios.post('https://api.fonnte.com/send', {
                 target: sender,
                 type: 'sticker',
-                url: url
+                url: mediaUrl
             }, {
-                headers: { 'Authorization': FONNTE_TOKEN }
+                headers: { 
+                    'Authorization': FONNTE_TOKEN,
+                    'Content-Type': 'application/json'
+                }
             });
         } catch (error) {
             console.error('Error:', error.message);
         }
     }
-
-    return res.status(200).send('OK');
 };
